b19dd57bae88135d78ec4c4583e42dbf9b40aefc
cherish <cherishzhang@sohu-inc.com>
2020-11-30 19:43:41 +0800

    a

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ from util import *
 if __name__ == '__main__':
 	# pa = os.environ["P_A"]
 	t = datetime.now().strftime("%H:%M")
-	pb = os.environ["P_B"]
+	pb = os.environ["PWD"]
 	send2server_jiang(pb, t)
