75ccbad4bbbbbf8a198fdef547841c2b48ce03b3
cherish <cherishzhang@sohu-inc.com>
2020-12-01 10:55:44 +0800

    a

--- main.py
+++ main.py
@@ -6,4 +6,4 @@ if __name__ == '__main__':
 	t = datetime.now().strftime("%H:%M")
 	pb = os.environ["PWD"]
 	#todo
-	send2server_jiang(pb, t)
+	# send2server_jiang(pb, t)
--- test.py
+++
@@ -1,7 +0,0 @@
-import os
-from datetime import datetime
-from util import *
-
-if __name__ == '__main__':
-	pb = os.environ["PWD"]
-	send2server_jiang(pb, 'test')
